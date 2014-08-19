165bbf435c50b1d43ba62ae62726e42b90e68699
James Dabbs <jamesdabbs@gmail.com>
2014-08-19 11:12:40 -0400

    Add Jobs model

---
+++ app/models/job.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: jobs
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text
+#  start_date  :date
+#  end_date    :date
+#  user_id     :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
+class Job < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140819142515) do
+ActiveRecord::Schema.define(version: 20140819151216) do
+
+  create_table "jobs", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
