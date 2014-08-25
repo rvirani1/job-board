940bf5e409fc9219856856d456079685594d3faf
David Askew <davidaskew@gmail.com>
2014-08-25 08:28:21 -0400

    Added basic show profile with gravitar support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,11 @@ module ApplicationHelper
       "No Company"
     end
   end
+  
+  def avatar_url(user)
+    #default = "#{root_url}images/guest.png"
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=200" #&d=#{CGI.escape(default)}"
+  end
+
 end
